ea4428e99f37bdc2fa38637bd6fea0eb1c13241f
Никита <Skobelev.nikita12345@mail.ru>
2023-11-01 10:40:32 +0700

    Создание страниц с детальным представлением книг

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -13,9 +13,6 @@ class Genre(models.Model):
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
-
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author is a string rather than an object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
@@ -29,8 +26,8 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        """Returns the URL to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
+            """Returns the url to access a particular book instance."""
+            return reverse('catalog:book-detail', args=[str(self.id)])
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -71,9 +68,9 @@ class Author(models.Model):
     class Meta:
         ordering = ['last_name', 'first_name']
 
-    def get_absolute_url(self):
-        """Returns the URL to access a particular author instance."""
-        return reverse('author-detail', args=[str(self.id)])
+    # def get_absolute_url(self):
+    #     """Returns the URL to access a particular author instance."""
+    #     return reverse('catalog:author-detail', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""
